74ff76ff81b15070a24954e1f7b363000bd0162e
yezhaoyu <zhaoyu.ye@vce.com>
2019-11-28 23:02:53 +0800

    fix permission issue

--- app/main_page_api.py
+++ app/main_page_api.py
@@ -5,6 +5,7 @@ Created on May 16, 2019
 
 '''
 import os
+import sys
 from datetime import datetime
 
 from flask import request, render_template, redirect, jsonify
@@ -48,7 +49,12 @@ def main_page_api(app):
             file_name = file.filename
             MiLog.info("file name : %s" % file_name)
             now_time = datetime.now()
-            file.save(root_path()+"\\app\\static\\storage_file\\%s"%file_name)
+            if 'linux' in sys.platform:
+                file.save(os.path.join("home", "zhaoyu", "mipi", "data", "storage_file", file_name))
+            else:
+                file.save(root_path()+"\\app\\static\\storage_file\\%s"%file_name)
+
+
             finish_time = datetime.now()
             spend_time = finish_time - now_time
             MiLog.info("finished in : %s seconds" % spend_time.seconds)
@@ -62,7 +68,11 @@ def main_page_api(app):
     @app.route('/storage/get_file', methods=['GET'])
     def get_file():
 
-        dir_list =  os.listdir(os.path.join(root_path(), "app", "static", "storage_file"))
+        if 'linux' in sys.platform:
+            mkdir = os.popen("mkdir -p /home/zhaoyu/mipi/data/storage_file").read()
+            dir_list = os.listdir(os.path.join("home", "zhaoyu",  "mipi", "data",  "storage_file"))
+        else:
+            dir_list = os.listdir(os.path.join(root_path(), "app", "static", "storage_file"))
         return_json = list()
         for each_file in dir_list:
             if ".jpg"  in each_file.lower() or ".png" in each_file.lower():
